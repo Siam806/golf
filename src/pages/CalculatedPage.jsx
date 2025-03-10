afd3dfeecf5daa7fcee0afb7f9ab83a29ff257c2
Lennard Gerst <lennard.gerst@gmail.com>
2025-03-10 15:51:13 +0100

    EGA Form anbindung an Local Storage. Sichtbar über ResultsPage, Calculated Page angepasst, []-> {} in ResultsPage

--- src/pages/CalculatedPage.jsx
+++ src/pages/CalculatedPage.jsx
@@ -4,6 +4,15 @@ import { Link, useLocation } from 'react-router-dom';
 export default function CalculatedPage() {
     const location = useLocation();
     const result = location.state?.result || "Keine Berechnung durchgeführt";
+    const scoreDifferential = location.state?.scoreDifferential || "Keine Berechnung durchgeführt";
+    const saveRound = () => {
+        console.log("Runde gespeichert!" + round);
+        // Hier kannst du die Daten speichern (z. B. in einer API oder im LocalStorage)
+        
+    };
+    
+    // Funktion global verfügbar machen
+    window.saveRound = saveRound;
 
     return (
         <>
@@ -16,7 +25,18 @@ export default function CalculatedPage() {
             <div className="text-center">
                 <h2 className="text-xl font-bold">Neues Handicap</h2>
                 <p className="text-3xl font-extrabold mt-4">{result}</p>
-            </div>
+            </div>   
+            <div className="text-center">
+                <h2 className="text-xl font-bold">Score Differential</h2>
+                <p className="text-3xl font-extrabold mt-4">{scoreDifferential}</p>
+            </div>   
+            <button 
+                className="bg-green-600 px-6 py-2 rounded-lg text-white font-bold hover:bg-green-700 transition mt-6"
+                onClick={() => window.saveRound && window.saveRound()}
+                >       
+                Speichern
+            </button>
+     
         </>
     );
 }
