5c175037752c64e58b176e02f191b121badaa437
Ömer-Faruk Tüfek <Oemer-Faruk.Tuefek@telekom.de>
2025-03-18 17:37:37 +0100

    added userHandicap in MemberItem, ResultsPage can now show user-related data,

--- src/components/MemberItem.jsx
+++ src/components/MemberItem.jsx
@@ -6,14 +6,14 @@ export default function MemberItem({ member }) {
 
   const handleClick = () => {
     // Übergibt nur die E-Mail des Spielers
-    navigate("/results", { state: { userEmail: member.userEmail } });
+    navigate("/results?userEmail=" + member.userEmail);
   };
 
   return (
     <div className="flex gap-5 items-center justify-between my-4 text-[14px] mr-4">
       <div className="flex-1 flex flex-row justify-between gap-5 border border-gray-300 p-5 rounded-lg text-nowrap overflow-x-auto">
         <span>🧑 {member.userName}</span>
-        <span>⛳ {50}</span>
+        <span>⛳ {member.userHandicap ?? ""}</span>
         <span>📧 {member.userEmail}</span>
       </div>
       <button onClick={handleClick} className="bg-green-500 text-white/90 p-2 rounded-lg hover:bg-green-700">
