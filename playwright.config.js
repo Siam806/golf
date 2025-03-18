451ac5a7df235632fe5e8fe9160363ba9d69179a
Ömer-Faruk Tüfek <Oemer-Faruk.Tuefek@telekom.de>
2025-03-18 19:12:45 +0100

    added some playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    viewport: { width: 1900, height: 1200 },
+    launchOptions: {
+      slowMo: 500,
+    },
   },
 
   /* Configure projects for major browsers */
